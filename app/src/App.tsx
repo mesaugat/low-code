5ba68b4663ff5e1579b2412b293e583ac7c998ea
Saugat Acharya <mesaugat@gmail.com>
2024-04-05 21:59:10 +0545

    Add basic treemap

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,36 @@
-import './App.css';
+import { TreeMap, TreeMapSeries } from 'reaviz';
+
+const data = [
+  {
+    key: 'backend',
+    data: [
+      {
+        key: 'src/server.js',
+        data: 100
+      },
+      {
+        key: 'src/util.js',
+        data: 20
+      }
+    ]
+  },
+  {
+    key: 'frontend',
+    data: [
+      {
+        key: 'src/App.js',
+        data: 10
+      },
+      {
+        key: 'src/core.js',
+        data: 20
+      }
+    ]
+  }
+];
 
 function App() {
-  return <>Hello from low-code visualizer!</>;
+  return <TreeMap height={450} width={450} data={data} series={<TreeMapSeries colorScheme="orrd" />} />;
 }
 
 export default App;
