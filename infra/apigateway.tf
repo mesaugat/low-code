d22c90035ae236c37f3c8ea82cfe889e8c64a02f
pranavpudasaini <pranavpudasaini@users.noreply.github.com>
2024-04-05 20:53:29 +0545

    refactor: infrastructure AIO

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -17,11 +17,10 @@ resource "aws_apigatewayv2_integration" "ingest" {
   api_id           = aws_apigatewayv2_api.lowspot_http_apigw.id
   integration_type = "AWS_PROXY"
 
-  connection_type = "INTERNET"
-  description     = "Ingest Lambda Integration"
-  integration_uri = aws_lambda_function.lowspot_ingest.invoke_arn
-  # integration_method   = "POST"
-  # passthrough_behavior = "WHEN_NO_MATCH"
+  connection_type    = "INTERNET"
+  description        = "Ingest Lambda Integration"
+  integration_uri    = aws_lambda_function.lowspot_ingest.invoke_arn
+  integration_method = "POST"
 }
 
 
