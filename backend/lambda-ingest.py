77a313a3f998b320313b917b20e693cce205b94a
pranavpudasaini <pranavpudasaini@users.noreply.github.com>
2024-04-05 19:05:03 +0545

    feat: add ingest lambda

---
+++ backend/lambda-ingest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import boto3
+import os
+import datetime
+import json
+
+s3 = boto3.client('s3')
+
+def lambda_handler(ev, context):
+    event = json.loads(ev["body"])
+    file_content = event['content']
+    repo_name = event['github_url'].split('/')[-1]
+    current_timestamp = event['current_timestamp']
+    file_name = event['name']
+
+    s3.put_object(
+        Bucket='dump-to-s3-hackathon',
+        Key=os.path.join(repo_name, f'{current_timestamp}_{file_name}'),
+        Body=file_content.encode('utf-8')
+    )
+
+    return {
+        'statusCode': 200,
+        'body': f'File {file_name} uploaded successfully!'
+    }
