197627666b2e55aed81d5f5cc5387dfad6423f55
Saugat Acharya <mesaugat@gmail.com>
2024-04-05 23:32:09 +0545

    Basic treemap visualization --> colors are gg

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { TreeMap, TreeMapSeries } from 'reaviz';
+import { TreeMap, TreeMapLabel, TreeMapRect, TreeMapSeries } from 'reaviz';
 
 const data = [
   {
@@ -26,11 +26,72 @@ const data = [
         data: 20
       }
     ]
+  },
+  {
+    key: 'core',
+    data: [
+      {
+        key: 'src/util.js',
+        data: 10
+      },
+      {
+        key: 'src/jwt.js',
+        data: 20
+      },
+      {
+        key: 'src/haha.js',
+        data: 30
+      },
+      {
+        key: 'src/secret.js',
+        data: 40
+      }
+    ]
+  },
+  {
+    key: 'extension',
+    data: [
+      {
+        key: 'src/asd.js',
+        data: 10
+      },
+      {
+        key: 'src/wild.js',
+        data: 20
+      },
+      {
+        key: 'src/comedy.js',
+        data: 30
+      },
+      {
+        key: 'src/lol.js',
+        data: 40
+      }
+    ]
   }
 ];
 
 function App() {
-  return <TreeMap height={450} width={450} data={data} series={<TreeMapSeries colorScheme="orrd" />} />;
+  return (
+    <div className="container">
+      <div className="grid">
+        <TreeMap
+          height={600}
+          width={600}
+          data={data}
+          series={
+            <TreeMapSeries
+              colorScheme="pastel1"
+              rect={<TreeMapRect onClick={(event, data) => console.log('onClick', event, data)} />}
+              label={<TreeMapLabel wrap={true} fontSize={12} placement="middle" />}
+            />
+          }
+        />
+      </div>
+      <div className="separator"></div>
+      <div className="grid"></div>
+    </div>
+  );
 }
 
 export default App;
