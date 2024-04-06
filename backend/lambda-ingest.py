c8fe1e2ec8a9e016cc6837ac42be2e0c8cc2d08d
Saphall <safalsakha@gmail.com>
2024-04-06 15:25:39 +0545

    chore: gg ingest table

--- backend/lambda-ingest.py
+++ backend/lambda-ingest.py
@@ -28,53 +28,52 @@ def lambda_handler(event, context):
         cur.execute("SHOW TABLES LIKE 'ingestion_payload'")
         table_exists = cur.fetchone() is not None
 
-        if table_exists:
-            cur.execute("DROP TABLE default.ingestion_payload")
-        cur.execute("""
-            CREATE TABLE default.ingestion_payload (
-                id UUID DEFAULT generateUUIDv4(),
-                repo_url String,
-                repo_branch String,
-                repo_head String,
-                repo_user String,
-                changed_file String,
-                change_reason String,
-                range_start_line UInt64,
-                range_end_line UInt64,
-                is_dirty UInt8,
-                timestamp DateTime64(3, 'UTC')
-            ) ENGINE = MergeTree()
-            ORDER BY (id)
-            """)
+        if not table_exists:
+            cur.execute("""
+                CREATE TABLE default.ingestion_payload (
+                    id UUID DEFAULT generateUUIDv4(),
+                    repo_url String,
+                    repo_branch String,
+                    repo_head String,
+                    repo_user String,
+                    changed_file String,
+                    change_reason String,
+                    range_start_line UInt64,
+                    range_end_line UInt64,
+                    is_dirty UInt8,
+                    timestamp DateTime64(3, 'UTC')
+                ) ENGINE = MergeTree()
+                ORDER BY (id)
+                """)
 
         # Insert the request payload
         for item in request_body:
             item['timestamp'] = isoparse(item['timestamp'])
             cur.execute("""
-            INSERT INTO default.ingestion_payload (
-            change_reason,
-            changed_file,
-            is_dirty,
-            range_start_line,
-            range_end_line,
-            repo_branch,
-            repo_head,
-            repo_url,
-            repo_user,
-            timestamp
-            ) VALUES (
-                %(change_reason)s,
-                %(changed_file)s,
-                %(is_dirty)s,
-                %(range_start_line)s,
-                %(range_end_line)s,
-                %(repo_branch)s,
-                %(repo_head)s,
-                %(repo_url)s,
-                %(repo_user)s,
-                %(timestamp)s
-                )
-                """, item)
+                INSERT INTO default.ingestion_payload (
+                change_reason,
+                changed_file,
+                is_dirty,
+                range_start_line,
+                range_end_line,
+                repo_branch,
+                repo_head,
+                repo_url,
+                repo_user,
+                timestamp
+                ) VALUES (
+                    %(change_reason)s,
+                    %(changed_file)s,
+                    %(is_dirty)s,
+                    %(range_start_line)s,
+                    %(range_end_line)s,
+                    %(repo_branch)s,
+                    %(repo_head)s,
+                    %(repo_url)s,
+                    %(repo_user)s,
+                    %(timestamp)s
+                    )
+                    """, item)
 
         return {
         'statusCode': 200,
