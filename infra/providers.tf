c6812e0a608edc5dbc853286fab7b647e6b537f6
pranavpudasaini <pranavpudasaini@users.noreply.github.com>
2024-04-06 16:22:57 +0545

    fix: lint

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 5.44.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.44.0"
     }
     random = {
       source  = "hashicorp/random"
