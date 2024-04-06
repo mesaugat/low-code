537e49bf94d7aea0178cbe96d1bccc7513246c49
pranavpudasaini <pranavpudasaini@users.noreply.github.com>
2024-04-06 17:24:56 +0545

    fix: CORS

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -35,7 +35,7 @@ resource "aws_apigatewayv2_api" "lowspot_http_apigw" {
   name          = "lowspot"
   protocol_type = "HTTP"
   cors_configuration {
-    allow_origins = ["http://localhost:5173", "http://localhost:5174", aws_cloudfront_distribution.analysis.domain_name]
+    allow_origins = ["http://localhost:5173", "http://localhost:5174", "https://${aws_cloudfront_distribution.analysis.domain_name}"]
   }
 }
 
