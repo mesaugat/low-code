8843b513af439f2b6884fe99e97899ca1fcca51f
Saphall <safalsakha@gmail.com>
2024-04-06 09:02:03 +0545

    chore: update lambda ingest to dump json according to repo in S3

--- backend/lambda-ingest.py
+++ backend/lambda-ingest.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 
-import boto3
 import os
-import datetime
 import json
+import boto3
 
-s3 = boto3.client('s3')
+s3 = boto3.client("s3")
 
-def lambda_handler(ev, context):
-    event = json.loads(ev["body"])
-    file_content = event['content']
-    repo_name = event['github_url'].split('/')[-1]
-    current_timestamp = event['current_timestamp']
-    file_name = event['name']
 
-    s3.put_object(
-        Bucket='',
-        Key=os.path.join(repo_name, f'{current_timestamp}_{file_name}'),
-        Body=file_content.encode('utf-8')
-    )
+def lambda_handler(event, context):
+    event = json.loads(event["body"])
 
-    return {
-        'statusCode': 200,
-        'body': f'File {file_name} uploaded successfully!'
-    }
+    # Extract data from event
+    repo_name = event["repo"]
+    json_data = json.dumps(event)
+
+    # S3 bucket and file_key
+    bucket_name = "com.lowkey.dumpbucketwa"
+    file_key = os.path.join(repo_name, f"{repo_name}.json")
+
+    try:
+        # Check if the file exists
+        s3.head_object(Bucket=bucket_name, Key=file_key)
+
+        # If file exists, append new data to it
+        existing_data = (
+            s3.get_object(Bucket=bucket_name, Key=file_key)["Body"]
+            .read()
+            .decode("utf-8")
+        )
+        existing_json = json.loads(existing_data)
+        existing_json.append(event)
+        new_data = json.dumps(existing_json)
+
+    except s3.exceptions.ClientError:
+        # If file doesn't exist, create a new JSON
+        new_data = json.dumps([event])
+
+    # Save the updated/created data back to S3
+    s3.put_object(Bucket=bucket_name, Key=file_key, Body=new_data.encode("utf-8"))
+
+    return {"statusCode": 200, "body": f"File {file_key} uploaded successfully!"}
