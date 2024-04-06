1da7340feea7e1ffadaab8f4e4d46109f6a0ce1f
pranavpudasaini <pranavpudasaini@users.noreply.github.com>
2024-04-07 01:48:07 +0545

    fix: jese image

--- app/src/ChatBubble.jsx
+++ app/src/ChatBubble.jsx
@@ -5,7 +5,7 @@ const ChatBubble = (props) => {
 
   return (
     <div className="flex items-start gap-2.5">
-      <img className="w-8 h-8 rounded-full" src="../public/logo.png" alt="Jese image" />
+      <img className="w-8 h-8 rounded-full" src="../logo.png" alt="Jese image" />
       <div className="flex flex-col w-full leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
           <span className="text-sm font-semibold text-gray-900 dark:text-white">Low Code Bot</span>
