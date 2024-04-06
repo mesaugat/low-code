798ad8c0920bf211187d83cc4018c0bc9b2421d9
Saugat Acharya <mesaugat@gmail.com>
2024-04-06 09:02:17 +0545

    Add select

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,96 +1,31 @@
-import { TreeMap, TreeMapLabel, TreeMapRect, TreeMapSeries } from 'reaviz';
-
-const data = [
-  {
-    key: 'backend',
-    data: [
-      {
-        key: 'src/server.js',
-        data: 100
-      },
-      {
-        key: 'src/util.js',
-        data: 20
-      }
-    ]
-  },
-  {
-    key: 'frontend',
-    data: [
-      {
-        key: 'src/App.js',
-        data: 10
-      },
-      {
-        key: 'src/core.js',
-        data: 20
-      }
-    ]
-  },
-  {
-    key: 'core',
-    data: [
-      {
-        key: 'src/util.js',
-        data: 10
-      },
-      {
-        key: 'src/jwt.js',
-        data: 20
-      },
-      {
-        key: 'src/haha.js',
-        data: 30
-      },
-      {
-        key: 'src/secret.js',
-        data: 40
-      }
-    ]
-  },
-  {
-    key: 'extension',
-    data: [
-      {
-        key: 'src/asd.js',
-        data: 10
-      },
-      {
-        key: 'src/wild.js',
-        data: 20
-      },
-      {
-        key: 'src/comedy.js',
-        data: 30
-      },
-      {
-        key: 'src/lol.js',
-        data: 40
-      }
-    ]
-  }
-];
+import Header from './Header';
+import Select from './Select';
+import Visualizer from './Visualizer';
 
 function App() {
   return (
-    <div className="container">
-      <div className="grid">
-        <TreeMap
-          height={600}
-          width={600}
-          data={data}
-          series={
-            <TreeMapSeries
-              colorScheme="pastel1"
-              rect={<TreeMapRect onClick={(event, data) => console.log('onClick', event, data)} />}
-              label={<TreeMapLabel wrap={true} fontSize={12} placement="middle" />}
-            />
-          }
+    <>
+      <Header title="LowCode Visualizer" />
+      <p>
+        Group By:{' '}
+        <Select
+          options={[
+            { value: 'Folder', label: 'Folder' },
+            { value: 'User', label: 'User' },
+            { value: 'Edits', label: 'Edits' }
+          ]}
+          defaultValue="option1"
+          onChange={(e) => console.log(e.target.value)}
         />
+      </p>
+      <div className="container">
+        <div className="grid">
+          <Visualizer />
+        </div>
+        <div className="separator"></div>
+        <div className="grid"></div>
       </div>
-      <div className="separator"></div>
-      <div className="grid"></div>
-    </div>
+    </>
   );
 }
 
